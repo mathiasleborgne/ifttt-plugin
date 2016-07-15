c30754b6df630722694dc43b6472c17221196cce
mathiasleborgne <mathias.leborgne@gmail.com>
2016-07-15 12:12:55 +0200

    API: placeholder

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -3,7 +3,7 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
+from flask import render_template, url_for
 from FlaskWebProject import app
 
 @app.route('/')
@@ -15,3 +15,12 @@ def home():
         title='Home Page',
         year=datetime.now().year,
     )
+
+@app.route('/articles')
+def api_articles():
+    return 'List of ' + url_for('api_articles')
+
+
+@app.route('/articles/<articleid>')
+def api_article(articleid):
+    return 'You are reading ' + articleid
--- trigger_custom_api.py
+++ trigger_custom_api.py
@@ -1,5 +1,13 @@
 import urllib2
 
+import argparse
+
+parser = argparse.ArgumentParser(description="Check server stuff")
+parser.add_argument("--local", action="store_true",
+                         help="Check API on local server")
+args = parser.parse_args()
+
+
 def send_url(url):
     req = urllib2.Request(url)
     handle = urllib2.urlopen(req)
@@ -7,4 +15,7 @@ def send_url(url):
 
 # print send_url("http://127.0.0.1:5000/")
 # print send_url("http://10.0.164.222:9090/")
-print send_url("http://syllogist.azurewebsites.net/articles")
+if args.local:
+    print send_url("http://localhost:5555/articles")
+else:
+    print send_url("http://syllogist.azurewebsites.net/articles")
